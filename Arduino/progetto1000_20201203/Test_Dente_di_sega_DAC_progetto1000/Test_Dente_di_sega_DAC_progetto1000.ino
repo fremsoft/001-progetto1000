d62daf5a9f52970f6d3214ac8fd4561dcaa8b702
fremsoft <41731090+fremsoft@users.noreply.github.com>
2020-12-06 02:09:16 +0100

    TEST DAC

---
+++ Arduino/progetto1000_20201203/Test_Dente_di_sega_DAC_progetto1000/Test_Dente_di_sega_DAC_progetto1000.ino
@@ -0,0 +1,53 @@
+/* Generatore di onda a dente di sega su Arduino Mega
+ *  con Convertitore Digitale Analogico (DAC R-2R)
+ *  e amplificatore LM386N.
+ *  
+ *  03-12-2020 - Emanuele Frisoni
+ */
+
+#define MODO_PARALLELO 0
+
+int segnale;
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  
+  /* PORT-L PINS */
+  pinMode(42, OUTPUT); /* PORT - L7 */ 
+  pinMode(43, OUTPUT);
+  pinMode(44, OUTPUT);
+  pinMode(45, OUTPUT);
+  pinMode(46, OUTPUT);
+  pinMode(47, OUTPUT);
+  pinMode(48, OUTPUT);
+  pinMode(49, OUTPUT); /* PORT - L0 */ 
+
+  segnale = 0;
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+  segnale = (segnale + 1) & 0xFF;
+
+#if MODO_PARALLELO 
+   
+  PORTL = segnale;
+
+#else
+
+  digitalWrite(49, bitRead(segnale, 0));
+  digitalWrite(48, bitRead(segnale, 1));
+  digitalWrite(47, bitRead(segnale, 2));
+  digitalWrite(46, bitRead(segnale, 3));
+  digitalWrite(45, bitRead(segnale, 4));
+  digitalWrite(44, bitRead(segnale, 5));
+  digitalWrite(43, bitRead(segnale, 6));
+  digitalWrite(42, bitRead(segnale, 7));
+
+#endif
+
+  delayMicroseconds( 10 );
+      
+}
---
+++ Arduino/progetto1000_20201203/Test_Sinusoide_DAC_progetto1000/Test_Sinusoide_DAC_progetto1000.ino
@@ -0,0 +1,43 @@
+/* Generatore di onda sinusoidale su Arduino Mega
+ *  con Convertitore Digitale Analogico (DAC R-2R)
+ *  e amplificatore LM386N.
+ *  La sinusoide viene memorizzata su un Array a 
+ *  360 punti  (un valore per ogni grado della 
+ *  circonferenza) in modo da ridurre al massimo 
+ *  il carico computazionale del microprocessore. 
+ *  
+ *  03-12-2020 - Emanuele Frisoni
+ */
+
+int segnale[360];
+int angolo;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+  /* PORT-L PINS */
+  pinMode(42, OUTPUT); /* PORT - L7 (MSb) */ 
+  pinMode(43, OUTPUT);
+  pinMode(44, OUTPUT);
+  pinMode(45, OUTPUT);
+  pinMode(46, OUTPUT);
+  pinMode(47, OUTPUT);
+  pinMode(48, OUTPUT);
+  pinMode(49, OUTPUT); /* PORT - L0 (LSb) */ 
+
+  for (angolo = 0; angolo < 360; angolo += 1)
+  {
+    segnale[angolo] = 128 + (126 * sin( (angolo * 6.28) / 360 ) );
+  }
+
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  
+   angolo ++; if (angolo >= 360) { angolo = 0; }
+   PORTL = segnale[angolo];
+
+   /* delay(100); */ 
+      
+}
